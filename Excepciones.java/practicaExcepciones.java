8db4e02503242aadb4fc21f34fe0c14fa4910382
Keeners Navarro//KINGROCK2042 <130202458+KINGROCK2005@users.noreply.github.com>
2023-10-28 18:11:23 -0600

    Create practicaExcepciones.java

---
+++ Excepciones.java/practicaExcepciones.java
@@ -0,0 +1,32 @@
+// import java.util.InputMismatchException;
+// import java.util.Scanner;
+// public class appp2 {
+    
+//     public static void main(String[] args){
+//         int x = 1;
+//         // System.out.println(pedirEnterosConValidaciones());
+//         System.out.println(pedirEnteroConExcepciones());
+
+//     }
+//     public static int pedirEnterosSinExcepciones(){
+//         Scanner lector = new Scanner(System.in);
+//         System.out.println("Ingrese un valor entero: ");
+//         return lector.nextInt();
+
+//     }
+//     public static int pedirEnterosConValidaciones(){
+//         Scanner lector = new Scanner(System.in);
+//         boolean esValido = true;
+//         System.out.println("Ingrese un valor entero: ");
+//         String entrada = lector.next();
+//         for(int i = 0; i < entrada.length(); i++){
+//             if(!Character.isDigit(entrada.charAt(i))){//funcion para detectar un valor numerico en una cadena!!!!!!!!
+//                 esValido = false;
+//                 break;
+//             }
+//         }
+//         if(esValido){
+//             return Integer.parseInt(entrada);
+//         }
+//         return 0;
+//     }
