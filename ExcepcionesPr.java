9b4efb91b37dd98b136c64743f25a4564ea10d6f
Keeners Navarro//KINGROCK2042 <130202458+KINGROCK2005@users.noreply.github.com>
2023-10-28 18:16:48 -0600

    Update ExcepcionesPr.java

--- ExcepcionesPr.java
+++ ExcepcionesPr.java
@@ -1,27 +1,27 @@
-// public class ExcepcionesPr {
-//     public static void main(String[] args) throws Exception {
-//         int x = 98;
-//         int y = 0;
-//         //System.out.println(dividirSinExcepxiones(x,y));
-//         //System.out.println(dividirConValidaciones(x, y));
-//         System.out.println(dividirConExcepciones(x, y));
-//     }
+ public class ExcepcionesPr {
+     public static void main(String[] args) throws Exception {
+         int x = 98;
+         int y = 0;
+         System.out.println(dividirSinExcepxiones(x,y));
+         System.out.println(dividirConValidaciones(x, y));
+         System.out.println(dividirConExcepciones(x, y));
+     }
 
-//     public static int dividirSinExcepciones(int x, int y){
-//     return x/y;
-//     }
-//     public static int dividirConValidaciones(int x, int y){
-//         if(y != 0){
-//             return x/y;
-//         }else{
-//             return 0;
-//         }
-//     }
-//     public static int dividirConExcepciones(int x, int y){
-//             try{
-//                 return x/y;
-//             } catch(ArithmeticException e){
-//                 return 0;
-//             }
-//         }
-// }//3
+     public static int dividirSinExcepciones(int x, int y){
+     return x/y;
+     }
+     public static int dividirConValidaciones(int x, int y){
+         if(y != 0){
+             return x/y;
+         }else{
+             return 0;
+         }
+     }
+     public static int dividirConExcepciones(int x, int y){
+             try{
+                 return x/y;
+             } catch(ArithmeticException e){
+                return 0;
+             }
+         }
+ }
